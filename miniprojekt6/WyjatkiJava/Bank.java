8fea9299c87228249edd8090b64c8027703b7ed2
Tomasz <63928070+Kejdul@users.noreply.github.com>
2021-01-19 10:28:58 +0100

    Update Bank.java

--- miniprojekt6/WyjatkiJava/Bank.java
+++ miniprojekt6/WyjatkiJava/Bank.java
@@ -17,25 +17,41 @@ public class Bank implements SearchAccounts, SearchCustomers {
 
     @Override
     public Customer findByName(String name) throws CustomerNotFoundException {
-        
+        System.out.println("Szukanie kont po nazwe");
+        int a = 11;
+        int b = 6;
+        System.out.println("Tesotwanie funkcji");
+        System.out.println("findByName");
+        return new Customer();
             
     }
 
     @Override
     public Account findByNumber(int num) throws AccountNotFoundException {
-        
-
+        System.out.println("Szukanie kont po liczbie");
+        int a = 11;
+        int b = 6;
+        System.out.println("Tesotwanie funkcji");
+        System.out.println("findBynumber");
+        return new Account();
     }
 
     @Override
     public ArrayList<Account> findAllCustomerAccounts(Customer cust) throws AccountNotFoundException {
-        
-
-
+        System.out.println("Szukanie wszystkich customerow");
+        System.out.println("Tesotwanie funkcji");
+        System.out.println("findAllCustomerAccounts");
     }
 
     public static void main(String[] args) {
-        
+        System.out.println("Testowanie maina");
+        System.out.println("wypisywanie argów");
+        System.out.println("============");
+        System.out.println( args[0] );
+        System.out.println( args[1] );
+        System.out.println( args[2] );
+        System.out.println( args[3] );
+        System.out.println( args[4] );
 
       }
 }
